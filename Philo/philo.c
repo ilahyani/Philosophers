2a97df383adc9ce8f1faad6761845679dd783ace
ilahyani <lahyani799@gmail.com>
2022-05-11 07:36:03 +0100

    threads drill

--- Philo/philo.c
+++ Philo/philo.c
@@ -6,15 +6,25 @@
 /*   By: ilahyani <ilahyani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/30 10:35:53 by ilahyani          #+#    #+#             */
-/*   Updated: 2022/05/10 11:45:08 by ilahyani         ###   ########.fr       */
+/*   Updated: 2022/05/11 07:11:05 by ilahyani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	*routine()
+{
+	printf("this is a thread\n");
+	return NULL;
+}
+
 int	main(int argc, char** argv)
 {
+	pthread_t t1;
+	
 	if (error_check(argc, argv))
-		printf("Error\n");
+		printf("Error\n"); //EXIT HERE
+	pthread_create(&t1, NULL, &routine, NULL);
+	pthread_join(t1, NULL);
 	return (0);
 }
